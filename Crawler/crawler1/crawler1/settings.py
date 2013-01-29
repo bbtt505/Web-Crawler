03b22926c4bf10640c9df462aeb6620dc22bde5d
tbu <bbtt505@gmail.com>
2013-01-29 00:39:56 -0800

    add directory

---
+++ Crawler/crawler1/crawler1/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for crawler1 project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'crawler1'
+
+SPIDER_MODULES = ['crawler1.spiders']
+NEWSPIDER_MODULE = 'crawler1.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'crawler1 (+http://www.yourdomain.com)'
